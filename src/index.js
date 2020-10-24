607f663dc917de9f853e1014ca9f98068d496b6d
Mikita Masliyevich <mikita.masliyevich@gmail.com>
2020-10-24 23:25:28 +0300

    feat: add final version for this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0){
+        return false
+      } 
+    
+     let openArray = [];
+     let closeArray = []; 
+      for (let i = 0; i < bracketsConfig.length; i++){
+        openArray.push(bracketsConfig[i][0])
+        closeArray.push(bracketsConfig[i][1])
+      }
+     let stack = [];
+     for (let i = 0; i < str.length; i++){
+     if (openArray.indexOf(str[i]) !== -1) {
+       if (openArray.indexOf(str[i]) === closeArray.indexOf(str[i]) && str[i] === stack[stack.lenght-1]){
+         stack.pop()
+       } else {
+         stack.push(str[i])
+       }
+     } else if (closeArray.indexOf(str[i]) !== -1 && closeArray.indexOf(str[i]) ===  openArray.indexOf(stack[stack.lenght-1])) {
+       stack.pop()
+     } else {
+       return false
+     }
+     if (stack.length > 0){
+       return true
+     }else {
+       return false
+     }
+    }
 }
